b5c0727ed37d5264d1162c94b2b9896cefc21c7a
shwetadubey23 <shwetadubey0246@gmail.com>
2023-06-19 19:51:50 +0530

    project-using-props

--- src/App.js
+++ src/App.js
@@ -1,65 +1,25 @@
 import React from "react";
-import Card from "./Card";
-import cardData from "./CardData";
+import './index.css'
+import SlotM from "./SlotM";
 
 
-// function App() {     //  we have a simlpe method instid of useing this 
-//     return (
-//         <>
-//         <h3 className="heading_style">List of Netflix Series</h3>
-//             <Card imgsrc={cardData[0].imgsrc}
-//             title={cardData[0].title}
-//             sname={cardData[0].sname}
-//             link={cardData[0].link}
-//             />
-//             <Card imgsrc={cardData[1].imgsrc}
-//             title={cardData[1].title}
-//             sname={cardData[1].sname}
-//             link={cardData[1].link}
-//             />
-//             <Card imgsrc={cardData[2].imgsrc}
-//             title={cardData[2].title}
-//             sname={cardData[2].sname}
-//             link={cardData[2].link}
-//             />
 
-
-//         </>
-
-//     );
-// }
-
-// export default App
-
-//_________Simple Method using map on CardData module
-
-// function cardValue(value) {
-//     return (
-// <Card 
-// imgsrc={value.imgsrc}
-// title={value.title}
-// sname={value.sname}
-// link={value.link}
-// />
-//     )
-// }
-
-
-// ___________using Arrow function
 function App() {
     return (
         <>
-            <h3 className="heading_style">List of Netflix Series</h3>
-            {cardData.map((value) => {
-                return (
-                    <Card
-                        imgsrc={value.imgsrc}
-                        title={value.title}
-                        sname={value.sname}
-                        link={value.link}
-                    />
-                )
-            })}
+            <h1 className="heading_style"> 🎰 Welcome to 🎰 <span style={{ fontWeight: 'bold' }} > Slot machine game </span>
+
+            </h1>
+            <div className="slotmachine">
+              
+                <SlotM x='😄' y='😄' z='😄' />
+                <hr />
+                <SlotM x='😄' y='👼' z='⏰' />
+                <hr />
+                <SlotM x='⚖️' y='✈️' z='🥇' />
+               
+
+            </div>
         </>
     )
 }
