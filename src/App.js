c79660edd339bc42a89adb73eab802f9242dced0
shwetadubey23 <shwetadubey0246@gmail.com>
2023-06-29 20:18:41 +0530

    multiple-input-form

--- src/App.js
+++ src/App.js
@@ -4,36 +4,77 @@ import './index.css'
 /*Single source of trueth Ek hi vairiable ko multiple time access karna*/
 const App = () => {
 
-    const [initialName, setInitialName] = useState("");
-    const [firstName, setfirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [ fullName, setFullName] = useState("");
-
-    const onSubmit = (event) => {
-        event.preventDefault()
-        setLastName(initialName)
-        setFullName(firstName)
-    };
+    const [fullName, setfullName] = useState({
+        fname: '',
+        lname: '',
+        email: '',
+        phone: ''
+    });
 
     const inputEvent = (event) => {
-        setInitialName(event.target.value)
+        // console.log(event.target.value)
+        // console.log(event.target.name)
 
-    }
+        const { value, name } = event.target;
+
+        setfullName((preValue) => {
+            if (name === 'fname') {
+                return {
+                    fname: value,
+                    lname: preValue.lname,
+                    email: preValue.email,
+                    phone: preValue.phone
+                }
+            } else if (name === 'lname') {
+                return {
+                    fname: preValue.fname,
+                    lname: value,
+                    email: preValue.email,
+                    phone: preValue.phone
+                }
+            } else if (name === 'email') {
+                return {
+                    fname: preValue.fname,
+                    lname: preValue.lname,
+                    email: value,
+                    phone: preValue.phone
+                }
+            } else if (name === 'phone') {
+                return {
+                    fname: preValue.fname,
+                    lname: preValue.lname,
+                    email: preValue.email,
+                    phone: value
+                }
+            }
+
+        });
+    };
 
 
-    const secondInputEvent = (event) => {
-setfirstName(event.target.value)
-    }
+    const onSubmit = (event) => {
+        event.preventDefault();
+        alert("form Submitted")
+
+    };
+
+
 
     return (
         <>
-            <div>
+            <div >
                 <form onSubmit={onSubmit}>
-                    <div className='div_style'>
-                        <h1 >  Hello {lastName} {fullName} </h1>
+                    <div className='div_style' >
+                        <h1 >  Hello {fullName.fname} {fullName.lname} </h1>
+                        <p>{fullName.email}</p>
+                        <p>{fullName.phone}</p>
 
-                        <input className='input' type='text' placeholder='Enter your firstName' onChange={inputEvent} value={initialName} />
-                        <input className='input' type='text' placeholder='Enter your lasttName' onChange={secondInputEvent} value={firstName} />
+                        <input className='input' type='text' placeholder='Enter your firstName' name="fname" onChange={inputEvent}
+                            value={fullName.fname} />
+                        <input className='input' type='text' placeholder='Enter your lasttName' name="lname" onChange={inputEvent} value={fullName.lname} />
+                        <input className='input' type='email' placeholder='Enter your email' name="email" onChange={inputEvent} value={fullName.email} />
+                        <input className='input' type='number' placeholder='Enter your phone' name="phone" onChange={inputEvent} value={fullName.phone} />
+
                         <br />
 
                         <button className='button' type="submit" > Submit </button>
