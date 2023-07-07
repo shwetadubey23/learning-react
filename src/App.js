26b337b75fb6a13f5c6f3bc3c116350b49880c8c
shwetadubey23 <shwetadubey0246@gmail.com>
2023-07-07 20:37:45 +0530

    modify

--- src/App.js
+++ src/App.js
@@ -18,37 +18,10 @@ const App = () => {
         const { value, name } = event.target;
 
         setfullName((preValue) => {
-            if (name === 'fname') {
-                return {
-                    fname: value,
-                    lname: preValue.lname,
-                    email: preValue.email,
-                    phone: preValue.phone
-                }
-            } else if (name === 'lname') {
-                return {
-                    fname: preValue.fname,
-                    lname: value,
-                    email: preValue.email,
-                    phone: preValue.phone
-                }
-            } else if (name === 'email') {
-                return {
-                    fname: preValue.fname,
-                    lname: preValue.lname,
-                    email: value,
-                    phone: preValue.phone
-                }
-            } else if (name === 'phone') {
-                return {
-                    fname: preValue.fname,
-                    lname: preValue.lname,
-                    email: preValue.email,
-                    phone: value
-                }
-            }
-
-        });
+           return {
+            ...preValue, 
+            [name] : value
+        }});
     };
 
 
