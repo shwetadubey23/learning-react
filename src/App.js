4f86ab748851e59c73602d68004339eeddc43bb0
shwetadubey23 <shwetadubey0246@gmail.com>
2023-07-13 14:03:20 +0530

    create-card-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 import React from 'react'
-import Accordion from './Component/accordion/Accordion'
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 
 
 
 export default function App() {
-  
+
   return (
     <>
-    <div >
-<Accordion/>
-    </div>
+    
+        <h1 className='text-center text-danger my-5' > Welcome To My Tech World </h1>
+        <div className="container">
+  <div className="row">
+    <div className="col-sm"> <div className="card" style={{width: "18rem"}}>
+  <img src="https://picsum.photos/200/301" className="card-img-top" alt="..." height={"200px"}/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div> </div>
+    <div className="col-sm"> <div className="card" style={{width: "18rem"}}>
+  <img src="https://picsum.photos/201/300" className="card-img-top" alt="..." height="200px"/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div> </div>
+    <div className="col-sm"> <div className="card" style={{width: "18rem"}}>
+  <img src="https://picsum.photos/202/300" className="card-img-top" alt="..." height= "200px" />
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div> </div>
+  </div>
+</div>
     </>
   )
 }
