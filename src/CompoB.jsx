612b7ceba97a4f84b93f207363eccad827270222
shwetadubey23 <shwetadubey0246@gmail.com>
2023-07-14 22:54:35 +0530

    learn-useing-useContext

--- src/CompoB.jsx
+++ src/CompoB.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import CompoC from './CompoC'
+import React, { useContext } from 'react'
+import { FirstName, LastName } from './App'
 
  function CompoB() {
+    const fname = useContext(FirstName)   // Reduse the code with useContext 
+    const lname = useContext(LastName)    // same code Component C (CompoC) pe hai but without useContext
   return (
     <div>
-        <CompoC/>
+       <h1> My Name is {fname} {lname} </h1>
     </div>
   )
 }
