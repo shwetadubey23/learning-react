6eb50d45ee7b40af8cc5aa118051ba14ef31029b
shwetadubey23 <110589451+shwetadubey23@users.noreply.github.com>
2023-07-17 00:15:34 +0530

    Update Error.jsx
    Show Error Page

--- src/Error.jsx
+++ src/Error.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Error = () => {
   return (
-    <div>
-       <h1> Opps! Page Not Found </h1>
-
+   <div className='setStyle2'>
+      <h1> 404  </h1>
+      <p> Opps!! Page Not Found</p>
+      <Link to='/'>Go Back</Link>
        </div>
   )
 }
